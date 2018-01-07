a01e830a68e5de30f91677da192e959bffa95a49
juan hurtado <juanhurtado@mbp-de-juan.mshome.net>
2018-01-07 14:04:49 -0500

    Initial Commit

---
+++ SwiftBasics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBasics
+//
+//  Created by juan hurtado on 7/01/18.
+//  Copyright © 2018 juan hurtado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
